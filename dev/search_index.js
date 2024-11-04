abf4ffa8f63e80d476fdac1ac3ac9368af6f31ed
Documenter.jl <documenter@juliadocs.github.io>
2024-11-04 06:03:24 +0000

    build based on 8b139cc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MinecraftDataStructures","category":"page"},{"location":"#MinecraftDataStructures","page":"Home","title":"MinecraftDataStructures","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MinecraftDataStructures.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MinecraftDataStructures]","category":"page"}]
+}
