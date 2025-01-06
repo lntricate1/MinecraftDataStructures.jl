309c45c8771ad10d346954acfd08d2c89fc41cf1
lntricate1 <intricatebread@gmail.com>
2025-01-06 05:42:00 -0700

    Add BlockStateDict

--- src/MinecraftDataStructures.jl
+++ src/MinecraftDataStructures.jl
@@ -2,7 +2,8 @@ module MinecraftDataStructures
 
 using PooledArrays
 
-export Block, BlockState, AbstractBlockState, CompressedPalettedContainer
+export Block, BlockStateVector, BlockStateDict, AbstractBlockState, CompressedPalettedContainer
+export block_id, is_air
 
 abstract type AbstractBlockState end
 
@@ -10,47 +11,69 @@ struct Block <: AbstractBlockState
   id::String
 end
 
-struct BlockState <: AbstractBlockState
+struct BlockStateVector <: AbstractBlockState
   id::String
   properties::Vector{Pair{String, String}}
 end
 
+struct BlockStateDict <: AbstractBlockState
+  id::String
+  properties::Dict{String, String}
+end
+
+@inline block_id(block::Block) = block.id
+@inline block_id(block::BlockStateVector) = block.id
+@inline block_id(block::BlockStateDict) = block.id
+@inline is_air(block::AbstractBlockState) = block_id(block) == "minecraft:air"
+
 Base.:(==)(a::Block, b::Block) = a.id == b.id
-Base.:(==)(a::BlockState, b::BlockState) = a.id == b.id && a.properties == b.properties
+Base.:(==)(a::BlockStateVector, b::BlockStateVector) = a.id == b.id && a.properties == b.properties
+Base.:(==)(a::BlockStateDict, b::BlockStateDict) = a.id == b.id && a.properties == b.properties
 Base.isequal(a::Block, b::Block) = a.id == b.id
-Base.isequal(a::BlockState, b::BlockState) = a.id == b.id && a.properties == b.properties
+Base.isequal(a::BlockStateVector, b::BlockStateVector) = a.id == b.id && a.properties == b.properties
+Base.isequal(a::BlockStateDict, b::BlockStateDict) = a.id == b.id && a.properties == b.properties
 Base.hash(a::Block, h::UInt64) = hash(a.id, h)
-Base.hash(a::BlockState, h::UInt64) = hash(a.id, hash(a.properties, h))
+Base.hash(a::BlockStateVector, h::UInt64) = hash(a.id, hash(a.properties, h))
+Base.hash(a::BlockStateDict, h::UInt64) = hash(a.id, hash(a.properties, h))
 
 Base.zero(::Type{AbstractBlockState}) = Block("minecraft:air")
 Base.zero(::AbstractBlockState) = Block("minecraft:air")
 Base.zero(::Type{Block}) = Block("minecraft:air")
 Base.zero(::Block) = Block("minecraft:air")
-Base.zero(::Type{BlockState}) = BlockState("minecraft:air", Pair{String, String}[])
-Base.zero(::BlockState) = BlockState("minecraft:air", Pair{String, String}[])
+Base.zero(::Type{BlockStateVector}) = BlockStateVector("minecraft:air", Pair{String, String}[])
+Base.zero(::BlockStateVector) = BlockStateVector("minecraft:air", Pair{String, String}[])
+Base.zero(::Type{BlockStateDict}) = BlockStateDict("minecraft:air", Dict{String, String}())
+Base.zero(::BlockStateDict) = BlockStateDict("minecraft:air", Dict{String, String}())
 
 Base.one(::Type{AbstractBlockState}) = Block("minecraft:stone")
 Base.one(::AbstractBlockState) = Block("minecraft:stone")
 Base.one(::Type{Block}) = Block("minecraft:stone")
 Base.one(::Block) = Block("minecraft:stone")
-Base.one(::Type{BlockState}) = BlockState("minecraft:stone", Pair{String, String}[])
-Base.one(::BlockState) = BlockState("minecraft:stone", Pair{String, String}[])
+Base.one(::Type{BlockStateVector}) = BlockStateVector("minecraft:stone", Pair{String, String}[])
+Base.one(::BlockStateVector) = BlockStateVector("minecraft:stone", Pair{String, String}[])
+Base.one(::Type{BlockStateDict}) = BlockStateDict("minecraft:stone", Dict{String, String}())
+Base.one(::BlockStateDict) = BlockStateDict("minecraft:stone", Dict{String, String}())
 
 Base.show(io::IO, lr::Block) = print(io, split(lr.id, ":")[end])
-Base.show(io::IO, lr::BlockState) = print(io, split(lr.id, ":")[end])
+Base.show(io::IO, lr::BlockStateVector) = print(io, split(lr.id, ":")[end])
+Base.show(io::IO, lr::BlockStateDict) = print(io, split(lr.id, ":")[end])
 
 struct CompressedPalettedContainer{T}
   palette::Vector{T}
   data::Array{Int64}
 end
 
-function CompressedPalettedContainer(uncompressed::PooledArray{<:AbstractBlockState}, min_bits::Int64)
+function CompressedPalettedContainer(uncompressed::PooledArray{T}, min_bits::Int64) where T
   wordsize = max(min_bits, ceil(Int, log2(length(uncompressed.pool))))
   CompressedPalettedContainer(uncompressed.pool,
     reinterpret.(Int64, BitArray((n - 1) >>> i & 1 == 1 for n in uncompressed.refs for i in 0:wordsize - 1).chunks))
 end
 
-function PooledArray(compressed::CompressedPalettedContainer, min_bits::Int64, size::NTuple{N, Int}) where N
+function PooledArray(compressed::CompressedPalettedContainer{T}, min_bits::Int64, length::Int) where T
+  return PooledArray(compressed, min_bits, (length,))
+end
+
+function PooledArray(compressed::CompressedPalettedContainer{T}, min_bits::Int64, size::NTuple{N, Int}) where {N, T}
   @inbounds begin
   wordsize = max(min_bits, ceil(Int, log2(length(compressed.palette))))
   data = ones(UInt32, size)
